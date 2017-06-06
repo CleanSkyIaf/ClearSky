f515f27b3c38465333e4352ba17f2d4a85e69cb3
CleanSkiIaf <cleanskyiaf@gmail.com>
2017-06-06 22:57:57 +0300

    db

--- app/src/main/java/com/example/clearsky/MainActivity.java
+++ app/src/main/java/com/example/clearsky/MainActivity.java
@@ -1,13 +1,43 @@
 package com.example.clearsky;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+
 public class MainActivity extends AppCompatActivity {
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Firebase.set
+        // Write a message to the database
+        //DB
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference();
+
+        Person person = new Person();
+        //dding values
+        person.setName("tzion");
+        person.setAddress("hadad");
+
+        //Storing values to firebase
+        //myRef.child("Person").setValue(person);
+        myRef.child("users").child("12").setValue(person);
+
     }
+
+
+
+
+
 }
