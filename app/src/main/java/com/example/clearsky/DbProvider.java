1d1ab9b197ac8771645e1cc6c2352383bcf22206
cleanskyiaf <cleanskyiaf@gmail.com>
2017-06-07 14:05:43 +0300

    node js

--- app/src/main/java/com/example/clearsky/DbProvider.java
+++ app/src/main/java/com/example/clearsky/DbProvider.java
@@ -10,6 +10,8 @@ import com.google.firebase.database.GenericTypeIndicator;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
 
 import static android.content.ContentValues.TAG;
@@ -42,25 +44,22 @@ public class DbProvider {
         currRef.setValue(obj);
     }
 
-    public void read() {
+    public void readAllReports(final ArrayList<Report> persons) {
         // Read from the database
-        myRef.addListenerForSingleValueEvent(new ValueEventListener() {
 
+        final ArrayList<Person> ReportsList = new ArrayList<>();
+
+        myRef.addListenerForSingleValueEvent(new ValueEventListener() {
 
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 // This method is called once with the initial value and again
                 // whenever data at this location is updated.
-                Person personsObject = dataSnapshot.child("users").child("12").getValue(Person.class);
 
-//                GenericTypeIndicator<List<Person>> t = new GenericTypeIndicator<List<Person>>() {};
-//                Object personsObj = dataSnapshot.child("users").getValue(t);
-//                List<Person> persons = dataSnapshot.child("users").getValue(t);
-             //   Person [] persons = dataSnapshot.child("users").getValue(Person.class);
-//                for(Person person : persons ) {
-//                    Log.d(TAG, "Value is: " + person.getName());
-//                }
-
+                List<Person> personList = new ArrayList<>();
+                for (DataSnapshot postSnapshot: dataSnapshot.child("users").getChildren()) {
+                    personList.add(postSnapshot.getValue(Person.class));
+                }
             }
 
 
---
+++ app/src/main/java/com/example/clearsky/Report.java
@@ -0,0 +1,62 @@
+package com.example.clearsky;
+
+/**
+ * Created by ofek13 on 07/06/2017.
+ */
+
+public class Report {
+
+    private String birdKind;
+    private String birdPlace;
+    private String birdAzimuth;
+    private String date;
+    private int birdCount;
+
+    public Report(String birdKind, String birdPlace, String birdAzimuth, String date, int birdCount) {
+        this.birdKind = birdKind;
+        this.birdPlace = birdPlace;
+        this.birdAzimuth = birdAzimuth;
+        this.date = date;
+        this.birdCount = birdCount;
+    }
+
+    public String getBirdKind() {
+        return birdKind;
+    }
+
+    public void setBirdKind(String birdKind) {
+        this.birdKind = birdKind;
+    }
+
+    public String getBirdPlace() {
+        return birdPlace;
+    }
+
+    public void setBirdPlace(String birdPlace) {
+        this.birdPlace = birdPlace;
+    }
+
+    public String getBirdAzimuth() {
+        return birdAzimuth;
+    }
+
+    public void setBirdAzimuth(String birdAzimuth) {
+        this.birdAzimuth = birdAzimuth;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public int getBirdCount() {
+        return birdCount;
+    }
+
+    public void setBirdCount(int birdCount) {
+        this.birdCount = birdCount;
+    }
+}
